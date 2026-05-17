d2329fbde5b6f78363288bf0a7c794169e7e0125
Crptk <144494482+crptk@users.noreply.github.com>
2026-05-16 22:19:08 -0400

    Small addons (#67)
    * Adjusted button styling
    
    * Updated contributors

--- frontend/src/components/menu/Credits.tsx
+++ frontend/src/components/menu/Credits.tsx
@@ -1,6 +1,14 @@
 import { open } from '@tauri-apps/plugin-shell';
 
 export default function Credits() {
+    const team = [
+        { name: 'Crptk', role: 'App Owner, Developer' },
+        { name: 'Netsuma', role: 'Export settings, UI Upgrades' },
+        { name: 'Moongetsu', role: 'Tons of new settings, Discord RPC, Menu revamp'},
+        { name: 'Lewis', role: 'Background processes, Heavy optimizations' },
+        { name: '0xkhaosoccured', role: 'Grid UI Fixes' },
+    ];
+
     return (
         <div className="panel menu-panel">
             <div className="patchnotes-header">
@@ -8,13 +16,13 @@ export default function Credits() {
                 <p>Learn about the people who made AMVerge come to life!</p>
             </div>
             <div className="credits-content">
-                <div className="credits-row">
-                    <p>Crptk</p>
-                </div>
-                <div className="credits-row">
-                    <p>Netsuma</p>
-                    <p>Moongetsu</p>
-                    <p>Lewis</p>
+                <div className="credits-row credits-team-list">
+                    {team.map((member) => (
+                        <p key={member.name} className="credits-team-member">
+                            <span className="credits-team-name">{member.name}</span>
+                            <span className="credits-team-role">{member.role}</span>
+                        </p>
+                    ))}
                 </div>
                 <div className="credits-row">
                     <h4>Contributors</h4>
